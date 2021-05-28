7dfd1fa72dd8c578bd629be1a789441dcb7d6ace
Martin <office@martinpagels.de>
2021-05-28 23:49:22 +0200

    Add globalStyle

---
+++ src/globalStyle.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: Open-Sans, Helvetica, Sans-Serif;
+    background-image: url("bg.jpg");
+     background-size: cover;
+    
+  }
+`;
+
+export default GlobalStyle;
